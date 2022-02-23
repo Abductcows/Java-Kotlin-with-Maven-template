98fe04fc01756c6c848de6a2e41e70ca496650e3
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2022-02-23 21:24:51 +0200

    Main workflow structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
+import java.util.List;
+
 public class Main {
 
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        System.out.println("Hello Java!");
-    }
+      // Read all Customers @George
+      final List<Customer> customers = FilesKt.readAllCustomers("a");
+      System.out.printf("Read %s customers%n", customers.size());
+        /*FilesKt.readAndDo("a", (liked, disliked) -> {
+            //TODO: next version
+        });*/
+
+      // Rank their ingredients @Chris
+
+      // Decide which ingredients to keep @Lef
+
+   }
 }
